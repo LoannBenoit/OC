aa2a036122f4d07e2c5e966029bd2f75e31f72e1
loann BENOIT <loann.benoit@hotmail.com>
2019-02-21 10:29:36 +0100

    Modifying Anniversaire folder for jQuery exercise

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,13 +1,38 @@
 $(document).ready(function() {
+// Adding the event fot the first div on the click
+  $('.first').on('click', function(e){
+    $('.click_me').css({'display':'none'});
 
-    var row2 = $('<div class="row deux"></div>');
-    $('.container').append(row2);
-    row2.css({'height':'120px'});
+      var row2 = $('<div class="row two"></div>');
+      $('.container').append(row2);
 
-    var div2 = $('<div class="col-lg-2 col-offset-6 second"></div>').text('You\'re a great friend');
-    div2.css({'height':'70px', 'background-color':'#DD75DD', 'border':'2px solid black'});
-    $('.deux').append(div2);
+      var div2 = $('<div class="col-lg-3 col-offset-6 second div_secondaires"></div>').text('You\'re a great friend');
+      div2.css({'background-color':'#f36aad'});
+      $('.two').append(div2);
 
-    
-    $('.second').css({'margin':'0px auto'});
+      // Event on click for the second div
+      div2.on('click', function(e){
+          var row3 = $('<div class="row three"></div>');
+          $('.container').append(row3);
+
+          var div3 = $('<div class="col-lg-3 col-offset-6 third div_secondaires"></div>').text('And I wish you');
+          div3.css({'background-color':'#28a3cc'})
+          $('.three').append(div3);
+          
+          // Event on click on the third
+          div3.on('click', function(e){
+              var row4 = $('<div class="row four"></div>');
+              $('.container').append(row4);
+          
+              var div4 = $('<div class="col-lg-3 col-offset-6 fourth div_secondaires"></div>').text('All the best in the world !');
+              div4.css({'background-color':'#ffb545'});
+              row4.append(div4);
+
+              // And the last event on click on the last div
+              div4.on('click', function(e){
+                $('.second, .third, .fourth').text('HAPPY BIRTHDAY !');
+              });
+          });
+      });
+    });
 });
