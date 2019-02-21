31951a81184f6ac0d85f2719d53e046d4c4224f0
BENOIT Loann <ET02832@zgie.ztech>
2019-02-21 17:54:49 +0100

    Adding CodeIgniter to the project and making a first test

---
+++ PHP/www/CodeIgniter/application/views/news/success.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>CodeIgniter Tutorial</title>
+    </head>
+    <body>
+        <h2>Congratulation !</h2>
+
+        <p>Your news has been successfully added !</p>
+    </body>
+</html>
