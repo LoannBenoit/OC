7a98843b471a5a6f89f0a4d75bba29583028f352
BENOIT Loann <ET02832@zgie.ztech>
2019-02-22 15:50:36 +0100

    Changing background color and adding final divs

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,37 +1,33 @@
 $(document).ready(function() {
+
+var div2 = $('#second');
+  div2.css({'background-color':'#fd78f7'});
+
+var div3 = $('#third');
+  div3.css({'background-color':'#28a3cc'});
+
+var div4 = $('#fourth');
+  div4.css({'background-color':'#ffb545'});
+
 // Adding the event fot the first div on the click
-  $('.first').on('click', function(e){
-    
-    $('.click_me').css({'display':'none'});
-    $('#second').css({'display':'block'});
-  });
+$('.first').on('click', function(e){
+  $('.click_me').css({'display':'none'});
+  $('#second').css({'display':'block'});
+});
+      
+      
+// Event for the second div on click
+div2.on('click', function(e){
+  div3.css({'display':'block'});
+});
 
-      // Event on click for the second div
-      var div2 = $('#second');
-      div2.on('click', function(e){
-
-          var row3 = $('<div class="row three"></div>');
-          $('.container').append(row3);
-
-          var div3 = $('<div class="col-lg-3 col-offset-6 third div_secondaires"></div>').show().fadeIn('slow').text('And I wish you');
-          $('.third').css({'background':'#28a3cc'});
-          $('.three').append(div3);
-          
-          // Event on click on the third
-          div3.on('click', function(e){
-              var row4 = $('<div class="row four"></div>');
-              $('.container').append(row4);
-          
-              var div4 = $('<div class="col-lg-3 col-offset-6 fourth div_secondaires"></div>').text('All the best in the world !');
-              div4.css({'background-color':'#ffb545'});
-              row4.append(div4);
-
-              // And the last event on click on the last div
-              div4.on('click', function(e){
-                $('.second, .third, .fourth').text('HAPPY BIRTHDAY !');
-
-              
-          });
-      });
+// Event for the third on click
+div3.on('click', function(e){
+    div4.css({'display':'block'});
+});
+    // And the last event on click on the last div
+    div4.on('click', function(e){
+      $('#second, #third, #fourth').text('HAPPY BIRTHDAY !');
     });
+    
 });
