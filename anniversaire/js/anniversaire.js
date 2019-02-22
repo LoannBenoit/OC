0d2753d4c9526f4de0688ea426d66ef0378cb97e
loann BENOIT <loann.benoit@hotmail.com>
2019-02-22 12:59:12 +0100

    Modifying Anniversaire folder

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
 // Adding the event fot the first div on the click
   $('.first').on('click', function(e){
+    
     $('.click_me').css({'display':'none'});
-
-      var row2 = $('<div class="row two"></div>');
-      $('.container').append(row2);
-
-      var div2 = $('<div class="col-lg-3 col-offset-6 second div_secondaires"></div>').text('You\'re a great friend');
-      div2.css({'background-color':'#f36aad'});
-      $('.two').append(div2);
+    $('#second').css({'display':'block'});
+  });
 
       // Event on click for the second div
+      var div2 = $('#second');
       div2.on('click', function(e){
+
           var row3 = $('<div class="row three"></div>');
           $('.container').append(row3);
 
-          var div3 = $('<div class="col-lg-3 col-offset-6 third div_secondaires"></div>').text('And I wish you');
-          div3.css({'background-color':'#28a3cc'})
+          var div3 = $('<div class="col-lg-3 col-offset-6 third div_secondaires"></div>').show().fadeIn('slow').text('And I wish you');
+          $('.third').css({'background':'#28a3cc'});
           $('.three').append(div3);
           
           // Event on click on the third
@@ -31,7 +29,8 @@ $(document).ready(function() {
               // And the last event on click on the last div
               div4.on('click', function(e){
                 $('.second, .third, .fourth').text('HAPPY BIRTHDAY !');
-              });
+
+              
           });
       });
     });
